c8a6fa9d8c959a9c3ae4c0b3d24c64eac1cf9ef9
Henri Remonen <henri@remonen.fi>
2024-07-01 17:10:04 +0300

    feat: add api call for getting featured posts

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -26,3 +26,7 @@ export function getAllPosts(): Post[] {
     .sort((post1, post2) => (post1.date > post2.date ? -1 : 1))
   return posts
 }
+
+export function getFeaturedPosts(): Post[] {
+  return getAllPosts().filter((post) => post.featured)
+}
