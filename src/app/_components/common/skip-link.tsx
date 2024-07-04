6eb11f9f2f8a64a97b3dec6795946a5d6a69507e
Henri Remonen <henri@remonen.fi>
2024-07-04 15:15:18 +0300

    feat: initial navbar components

---
+++ src/app/_components/common/skip-link.tsx
@@ -0,0 +1,14 @@
+interface SkipLinkProps {
+    href: string
+  }
+  
+  const SkipLink = ({ href }: SkipLinkProps) => (
+    <a
+      href={href}
+      className='sr-only translate-y-[-150%] transform focus:not-sr-only focus:absolute focus:left-1/2 focus:translate-y-0 focus:transition-transform '
+    >
+      Skip to content
+    </a>
+  )
+  
+  export default SkipLink
---
+++ src/app/_components/navbar/navbar.tsx
@@ -0,0 +1,26 @@
+import dynamic from 'next/dynamic'
+import Image from 'next/image'
+import SkipLink from '../common/skip-link'
+
+const DarkModeToggle = dynamic(() => import('../theme/dark-mode-toggle'), {
+  ssr: false,
+})
+
+const Navbar = () => (
+  <header
+    role='banner'
+    className='mx-auto flex items-center justify-between px-4 py-4 md:px-8 md:py-8 lg:px-12 lg:py-8'
+  >
+    <SkipLink href='#main-content' />
+
+    <div id='logo' className='flex justify-center gap-4 align-middle'>
+      <Image src='/misc/logo.svg' alt='hremonen logo' width={100} height={50}/>
+    </div>
+
+    <div className='flex items-center justify-center'>
+      <DarkModeToggle />
+    </div>
+  </header>
+)
+
+export default Navbar
