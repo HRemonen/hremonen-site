6333110db7c70bf86f0d6b5e313688eaa16dd7fa
Henri Remonen <henri@remonen.fi>
2024-07-04 17:19:55 +0300

    refactor: remove the logo from hero

--- src/app/_components/hero.tsx
+++ src/app/_components/hero.tsx
@@ -1,12 +1,10 @@
 import { BLOG_NAME } from '@/lib/constants'
-import Image from 'next/image'
 
 const HeroSection = () => (
   <section className='mb-16 mt-16 flex flex-col items-center md:mb-12 md:justify-between'>
     <h1 className='text-4xl font-bold leading-tight tracking-tighter md:pr-8 md:text-6xl'>
       {BLOG_NAME}.
     </h1>
-    <Image src='/misc/logo.svg' alt='hremonen logo' width={300} height={150}/>
   </section>
 )
 
