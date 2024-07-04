ece8deaf3bbf3776310afb221aab54c045f2f1a5
Henri Remonen <henri@remonen.fi>
2024-07-04 17:19:29 +0300

    feat: update the sun-icon

--- src/app/_components/theme/sun-icon.tsx
+++ src/app/_components/theme/sun-icon.tsx
@@ -1,21 +1,21 @@
+
 import React from 'react'
+
 
 const SunIcon = () => (
   <svg
     aria-hidden='true'
     tabIndex={-1}
-    className='group h-10 w-10 fill-current transition-colors duration-300 ease-in-out hover:animate-spin-xslow'
+    className='group h-8 w-8 transition-colors duration-300 ease-in-out hover:text-yellow-400 hover:fill-yellow-300'
     xmlns='http://www.w3.org/2000/svg'
-    viewBox='0 0 16 16'
+    viewBox='0 0 24 24'
+    strokeWidth="1.5"
+    fill="none"
+    strokeLinecap='round'
+    strokeLinejoin='round'
   >
-    <path
-      className='text-yellow-300'
-      d='M7 0h2v2H7zM12.88 1.637l1.414 1.415-1.415 1.413-1.413-1.414zM14 7h2v2h-2zM12.95 14.433l-1.414-1.413 1.413-1.415 1.415 1.414zM7 14h2v2H7zM2.98 14.364l-1.413-1.415 1.414-1.414 1.414 1.415zM0 7h2v2H0zM3.05 1.706 4.463 3.12 3.05 4.535 1.636 3.12z'
-    />
-    <path
-      className='text-yellow-300 group-hover:text-yellow-400'
-      d='M8 4C5.8 4 4 5.8 4 8s1.8 4 4 4 4-1.8 4-4-1.8-4-4-4Z'
-    />
+    <path d="M17 12C17 14.7614 14.7614 17 12 17C9.23858 17 7 14.7614 7 12C7 9.23858 9.23858 7 12 7C14.7614 7 17 9.23858 17 12Z" stroke="currentColor" />
+    <path d="M12 2C11.6227 2.33333 11.0945 3.2 12 4M12 20C12.3773 20.3333 12.9055 21.2 12 22M19.5 4.50271C18.9685 4.46982 17.9253 4.72293 18.0042 5.99847M5.49576 17.5C5.52865 18.0315 5.27555 19.0747 4 18.9958M5.00271 4.5C4.96979 5.03202 5.22315 6.0763 6.5 5.99729M18 17.5026C18.5315 17.4715 19.5747 17.7108 19.4958 18.9168M22 12C21.6667 11.6227 20.8 11.0945 20 12M4 11.5C3.66667 11.8773 2.8 12.4055 2 11.5" stroke="currentColor" />
   </svg>
 )
 
