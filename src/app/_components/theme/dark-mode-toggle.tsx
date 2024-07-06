22001d51b91293c63549896bab5c6c0fe74e96cc
Henri Remonen <henri@remonen.fi>
2024-07-06 11:34:00 +0300

    fix: dark-mode not working correctly with Tailwind

--- src/app/_components/theme/dark-mode-toggle.tsx
+++ src/app/_components/theme/dark-mode-toggle.tsx
@@ -14,7 +14,7 @@ const DarkModeToggle = () => {
       type='button'
       aria-label={theme === 'light' ? 'Toggle dark mode' : 'Toggle light mode'}
       className='ml-3 flex flex-col justify-center'
-      onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
+      onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}
     >
       {theme === 'light' ? <SunIcon /> : <MoonIcon />}
     </button>
