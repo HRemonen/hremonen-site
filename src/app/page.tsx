33243572d3c30aa2f96c5588b9b4c29b5ce6dbd6
Henri Remonen <henri@remonen.fi>
2024-07-07 16:04:07 +0300

    chore: remove Hero for now

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,12 @@
-import PageContainer from "@/app/_components/common/page-container";
-import FeaturedSection from "@/app/_components/featured";
-import AllPosts from "@/app/_components/post/posts";
-import HeroSection from "./_components/hero";
+import PageContainer from "@/app/_components/common/page-container"
+import FeaturedSection from "@/app/_components/featured"
+import AllPosts from "@/app/_components/post/posts"
 
 const Index = () => (
   <PageContainer>
-    <HeroSection />
     <FeaturedSection />
     <AllPosts />
   </PageContainer>
 )
-
 
 export default Index
