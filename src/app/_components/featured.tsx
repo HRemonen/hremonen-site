68ea02e3553f2e4c74768c7bccb3827182b99729
Henri Remonen <henri@remonen.fi>
2024-07-07 16:42:46 +0300

    style: update featured styles

--- src/app/_components/featured.tsx
+++ src/app/_components/featured.tsx
@@ -6,37 +6,39 @@ const FeaturedSection = () => {
   const featured = getFeaturedPost()
 
   if (!featured) return null
-  
+
   const { title, slug, coverImage } = featured
-  
+
   return (
-<section className='max-w-5xl mx-auto mb-16'>
-  <article key={`featured-post-${title}`} className='group relative flex flex-row mx-auto overflow-hidden'>
-    <Link
-      as={`/posts/${slug}`}
-      href='/posts/[slug]'
-      className='w-3/5 rounded-md transition-all duration-300 ease-in-out group-hover:w-full'
-    >
-      <Image
-        src={coverImage}
-        alt="" // As the image is decorative, an empty alt attribute is appropriate
-        width={1024}
-        height={420}
-        className='rounded-lg max-h-[420px] max-w-3/5 object-cover transition-all duration-500 ease-in-out group-hover:scale-150'
-      />
-      <div className='z-10 flex flex-col absolute left-[380px] bottom-0'>
-        <h2 className='relative h-[420px] flex items-center justify-center text-6xl transition-all duration-500 ease-in-out transform translate-y-0 group-hover:-translate-y-full'>
-          {title}
-        </h2>
-        <h2 className='absolute top-0 h-[420px] flex items-center justify-center text-6xl transition-all duration-500 ease-in-out transform translate-y-full group-hover:translate-y-0'>
-          {title}
-        </h2>
-      </div>
-    </Link>
-  </article>
-</section>
-
-
-)}
+    <section className='mx-auto mb-16 max-w-5xl'>
+      <article
+        key={`featured-post-${title}`}
+        className='duration-600 group relative mx-auto flex flex-row overflow-hidden transition-all ease-in-out'
+      >
+        <Link
+          as={`/posts/${slug}`}
+          href='/posts/[slug]'
+          className='duration-600 w-3/5 rounded-md transition-all ease-in-out group-hover:w-full'
+        >
+          <Image
+            src={coverImage}
+            alt='' // As the image is decorative, an empty alt attribute is appropriate
+            width={1024}
+            height={420}
+            className='max-w-3/5 duration-600 max-h-[420px] rounded-lg object-cover transition-all ease-in-out group-hover:scale-150'
+          />
+          <div className='duration-600 absolute bottom-0 left-[380px] z-10 flex flex-col transition-all ease-in-out'>
+            <h2 className='duration-600 relative flex h-[420px] translate-y-0 transform items-center justify-center text-6xl transition-all ease-in-out group-hover:-translate-y-full'>
+              {title}
+            </h2>
+            <h2 className='duration-600 absolute top-0 flex h-[420px] translate-y-full transform items-center justify-center text-6xl transition-all ease-in-out group-hover:translate-y-0'>
+              {title}
+            </h2>
+          </div>
+        </Link>
+      </article>
+    </section>
+  )
+}
 
 export default FeaturedSection
