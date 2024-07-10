898e44bc6f38c103d3a0aceec4e9c53e6fe8e483
Henri Remonen <henri@remonen.fi>
2024-07-10 18:48:03 +0300

    feat: revert featured page design to something easier

--- src/app/_pages/home/featured.tsx
+++ src/app/_pages/home/featured.tsx
@@ -13,39 +13,27 @@ const FeaturedSection = () => {
     <section className='mx-auto mb-16 max-w-5xl'>
       <article
         key={`featured-post-${title}`}
-        className='duration-600 group relative mx-auto flex flex-row overflow-hidden transition-all ease-in-out'
+        className='group relative mx-auto flex flex-row'
       >
         <Link
           as={`/posts/${slug}`}
           href='/posts/[slug]'
-          className='duration-600 rounded-md transition-all ease-in-out group-hover:w-full lg:w-3/5'
+          className='rounded-md md:w-4/5 lg:w-4/5'
         >
           <Image
             src={coverImage}
             alt='' // As the image is decorative, an empty alt attribute is appropriate
             width={1024}
             height={420}
-            className='lg:max-w-3/5 duration-600 max-h-[420px] rounded-lg object-cover transition-all ease-in-out lg:group-hover:scale-150'
+            className='md:max-w-4/5 lg:max-w-4/6 max-h-[420px] rounded-lg border-4 border-transparent object-cover group-hover:border-primary dark:group-hover:border-secondary'
           />
-          <div className='absolute left-[380px] z-10 flex flex-col items-end lg:bottom-0'>
-            <div className='duration-600 relative hidden h-[420px] translate-y-0 transform items-center justify-center text-7xl transition-all ease-in-out group-hover:-translate-y-full lg:flex'>
-              <h2 className='text-right font-semibold leading-tight'>
-                <span className='rounded-bl-2xl bg-[#fcfcfc] box-decoration-clone shadow-custom-light dark:bg-[#1f2028] dark:shadow-custom-dark'>
+          <div className='right-0 top-0 flex flex-col items-end sm:absolute sm:left-[30%] lg:left-[380px]'>
+            <div className='relative flex h-auto justify-end text-2xl sm:text-6xl lg:text-7xl'>
+              <h2 className='w-full font-light leading-tight sm:text-right sm:font-semibold'>
+                <span className='sm:rounded-bl-2xl sm:bg-[#fcfcfc] sm:box-decoration-clone sm:shadow-custom-light sm:dark:bg-[#1f2028] sm:dark:shadow-custom-dark'>
                   {title}
                 </span>
               </h2>
-            </div>
-            <div className='duration-600 absolute top-0 flex h-[420px] max-w-[14ch] translate-y-full transform flex-col items-end justify-start text-6xl transition-all ease-in-out group-hover:translate-y-0'>
-              <h2 className='text-right font-light leading-tight'>
-                <span className='rounded-bl-xl bg-[#fcfcfc] box-decoration-clone shadow-custom-light dark:bg-[#1f2028] dark:shadow-custom-dark'>
-                  {title}
-                </span>
-              </h2>
-              <div className='text-right'>
-                <span className='text-xl text-accent-text dark:text-accent-text-dark'>
-                  Read more
-                </span>
-              </div>
             </div>
           </div>
         </Link>
