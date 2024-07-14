0cff9bafa3276715a59159e181f3d57947148d3d
Henri Remonen <henri@remonen.fi>
2024-07-14 16:25:36 +0300

    chore: try prettier lint-staged fix

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -5,7 +5,12 @@ const buildEslintCommand = (filenames) =>
     .map((f) => path.relative(process.cwd(), f))
     .join(' --file ')}`
 
+const buildPrettierCommand = (filenames) =>
+  `prettier --write ${filenames
+    .map((f) => path.relative(process.cwd(), f))
+    .join(' ')}`
+
 module.exports = {
   '*.{js,jsx,ts,tsx}': [buildEslintCommand],
-  '*.{js,jsx,ts,tsx,json,md,yml,yaml}': ['prettier --write'],
+  '*.{js,jsx,ts,tsx,json,md,yml,yaml}': [buildPrettierCommand],
 }
