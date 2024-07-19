10c237266b83e31b6523c6a3c2ef474997b69a31
Henri Remonen <henri@remonen.fi>
2024-07-19 10:38:23 +0300

    feat: allow for raw html in markdown files -refs: #18

--- src/lib/markdownToHtml.ts
+++ src/lib/markdownToHtml.ts
@@ -6,12 +6,14 @@ import rehypeStringify from 'rehype-stringify'
 import rehypeSlug from 'rehype-slug'
 import rehypeAutolinkHeadings from 'rehype-autolink-headings'
 import rehypePrettyCode from 'rehype-pretty-code'
+import rehypeRaw from 'rehype-raw'
 
 export default async function markdownToHtml(markdown: string) {
   const result = await unified()
     .use(remarkParse)
     .use(remarkGfm)
-    .use(remarkRehype)
+    .use(remarkRehype, { allowDangerousHtml: true })
+    .use(rehypeRaw)
     .use(rehypeSlug)
     .use(rehypeAutolinkHeadings, { behavior: 'wrap' })
     .use(rehypePrettyCode, { theme: 'andromeeda' })
