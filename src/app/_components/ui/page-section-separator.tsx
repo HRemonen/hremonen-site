1956d674a9335465cbc84869f4802bbd58cd8a71
Henri Remonen <henri@remonen.fi>
2024-07-21 11:22:55 +0300

    refactor: renamed section separator component

---
+++ src/app/_components/ui/page-section-separator.tsx
@@ -0,0 +1,5 @@
+const PageSectionSeparator = () => (
+  <hr className='mb-12 mt-16 border-neutral-200' />
+)
+
+export default PageSectionSeparator
--- src/app/_components/ui/section-separator.tsx
+++
@@ -1,3 +0,0 @@
-const SectionSeparator = () => <hr className='mb-24 mt-28 border-neutral-200' />
-
-export default SectionSeparator
