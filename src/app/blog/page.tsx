2e8295ca239cf61788b2c4f70f1bee286857e01f
Henri Remonen <henri@remonen.fi>
2024-07-22 13:33:50 +0300

    style: update styles for the category links -refs: #25

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -21,15 +21,15 @@ const Blog = () => {
         <PageSubtitle type='custom'>
           <ul className='flex flex-wrap'>
             {Object.entries(postCategories).map(([category, count]) => (
-              <li key={category} className='mb-4 mr-4'>
+              <li key={category} className='mr-4 lg:mr-8'>
                 <Link
                   href={{
                     pathname: '/blog',
                     query: { category },
                   }}
-                  className='group inline-flex items-end duration-200'
+                  className='group inline-flex items-end outline-none duration-200'
                 >
-                  <p className='md:text-4vw leading-tighter text-balance font-sans text-2xl lowercase tracking-tight text-gray-500 transition dark:text-gray-200'>
+                  <p className='leading-tighter text-balance font-sans text-4xl lowercase tracking-tight text-gray-500 transition group-hover:text-text group-focus:text-text dark:text-gray-200 dark:group-hover:text-text-dark dark:group-focus:text-text-dark'>
                     {category}
                   </p>
                   <span className='mb-2 ml-1 text-sm text-gray-500 dark:text-gray-200'>
