957d1d72d3165ee5cd998688b26f32dc97831388
Henri Remonen <henri@remonen.fi>
2024-08-02 13:24:44 +0300

    style: update the focus and hover styles of the styled link component

--- src/app/_components/ui/styled-link.tsx
+++ src/app/_components/ui/styled-link.tsx
@@ -11,7 +11,7 @@ const StyledLink = ({ href, className, children }: StyledLinkProps) => (
   <Link
     href={href}
     className={clsx(
-      "after:duration-400 relative block w-fit after:absolute after:block after:h-[2px] after:w-full after:origin-center after:scale-x-0 after:bg-link after:transition after:content-[''] hover:text-link after:hover:scale-x-75 dark:after:bg-link-dark dark:hover:text-link-dark",
+      "relative block w-fit outline-none after:absolute after:block after:h-[2px] after:w-full after:origin-center after:scale-x-0 after:bg-link after:transition after:duration-700 after:content-[''] hover:text-link after:hover:scale-x-75 focus:text-link after:focus:scale-x-75 dark:after:bg-link-dark dark:hover:text-link-dark dark:focus:text-link-dark",
       className
     )}
   >
