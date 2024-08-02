6c41d2c41ffefd1528a37ecd4278cde01fab9fc8
Henri Remonen <henri@remonen.fi>
2024-08-02 13:14:43 +0300

    feat: update styled link to allow for additional styles

--- src/app/_components/ui/styled-link.tsx
+++ src/app/_components/ui/styled-link.tsx
@@ -1,14 +1,19 @@
+import clsx from 'clsx'
 import Link from 'next/link'
 
 interface StyledLinkProps {
   href: string
+  className?: string
   children: React.ReactNode
 }
 
-const StyledLink = ({ href, children }: StyledLinkProps) => (
+const StyledLink = ({ href, className, children }: StyledLinkProps) => (
   <Link
     href={href}
-    className="after:duration-400 relative block w-fit after:absolute after:block after:h-[2px] after:w-full after:origin-center after:scale-x-0 after:bg-link after:transition after:content-[''] hover:text-link after:hover:scale-x-75 dark:after:bg-link-dark dark:hover:text-link-dark"
+    className={clsx(
+      "after:duration-400 relative block w-fit after:absolute after:block after:h-[2px] after:w-full after:origin-center after:scale-x-0 after:bg-link after:transition after:content-[''] hover:text-link after:hover:scale-x-75 dark:after:bg-link-dark dark:hover:text-link-dark",
+      className
+    )}
   >
     <span className='flex items-center'>{children}</span>
   </Link>
