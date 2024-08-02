33f3111fd5b42d0456bb0be91ab858909cd21118
Henri Remonen <henri@remonen.fi>
2024-08-02 13:52:38 +0300

    fix: button link focus animation not working

--- src/app/_components/ui/button-link.tsx
+++ src/app/_components/ui/button-link.tsx
@@ -1,3 +1,5 @@
+import Link from 'next/link'
+
 interface ButtonLinkProps {
   linkText: string
   href: string
@@ -26,7 +28,7 @@ const ButtonLink = ({ linkText, href }: ButtonLinkProps) => (
       </defs>
     </svg>
 
-    <a
+    <Link
       href={href}
       className='group relative inline-flex'
       style={{ filter: 'url(#buttonFilter)' }}
@@ -34,30 +36,16 @@ const ButtonLink = ({ linkText, href }: ButtonLinkProps) => (
       <div className='relative inline-flex w-auto items-center justify-center overflow-hidden rounded-full bg-cta px-5 py-2 leading-tight text-text'>
         <p className='relative top-px inline-flex flex-shrink-0'>{linkText}</p>
       </div>
-      <div className='-ml-1 flex h-9 w-9 flex-shrink-0 transform items-center justify-center overflow-hidden rounded-full bg-cta transition-transform group-hover:translate-x-3 group-hover:rotate-45 group-focus:translate-x-3 group-focus:rotate-45' />
-    </a>
-    <div className='absolute right-0 top-0 flex h-9 w-9 transform items-center justify-center transition-transform group-hover:translate-x-3 group-hover:rotate-45 group-focus:translate-x-3 group-focus:rotate-45'>
-      <div className='relative overflow-hidden text-text'>
-        <div className='relative left-0 top-0 transform transition-transform'>
-          <svg
-            className='h-3 w-3 fill-current'
-            xmlns='http://www.w3.org/2000/svg'
-            viewBox='0 0 384 512'
-          >
-            <path d='M328 96h24v288h-48V177.9L81 401l-17 17-33.9-34 17-17 223-223H64V96h264z' />
-          </svg>
-        </div>
-        <div className='absolute left-0 top-0 -translate-x-full translate-y-full transform transition-transform'>
-          <svg
-            className='h-3 w-3 fill-current'
-            xmlns='http://www.w3.org/2000/svg'
-            viewBox='0 0 384 512'
-          >
-            <path d='M328 96h24v288h-48V177.9L81 401l-17 17-33.9-34 17-17 223-223H64V96h264z' />
-          </svg>
-        </div>
+      <div className='-ml-1 flex h-9 w-9 flex-shrink-0 transform items-center justify-center overflow-hidden rounded-full bg-cta text-text transition-transform group-hover:translate-x-3 group-hover:rotate-45 group-focus:translate-x-3 group-focus:rotate-45'>
+        <svg
+          className='h-3 w-3 fill-current'
+          xmlns='http://www.w3.org/2000/svg'
+          viewBox='0 0 384 512'
+        >
+          <path d='M328 96h24v288h-48V177.9L81 401l-17 17-33.9-34 17-17 223-223H64V96h264z' />
+        </svg>
       </div>
-    </div>
+    </Link>
   </div>
 )
 
