d2fad2b07195d62f9063389d5e1c8f47c1d03655
Henri Remonen <henri@remonen.fi>
2024-08-03 11:17:56 +0300

    style: post-card and post related component stylings to better fit very narrow screens

--- src/app/_components/ui/post-card.tsx
+++ src/app/_components/ui/post-card.tsx
@@ -10,7 +10,7 @@ interface PostCardProps {
 }
 
 const PostCard = ({ post }: PostCardProps) => (
-  <div className='mb-8 w-full lg:mb-12'>
+  <div className='mb-16 w-full md:mb-12'>
     <Link
       href={`/blog/${post.slug}`}
       passHref
@@ -23,18 +23,19 @@ const PostCard = ({ post }: PostCardProps) => (
         height={420}
         className='mb-2 max-h-[420px] rounded-md object-cover outline-none ring-link ring-offset-4 ring-offset-[#fcfcfc] transition duration-700 ease-in-out group-hover/recent-post:ring-2 group-focus/recent-post:ring-2 dark:ring-link-dark dark:ring-offset-[#1f2028] md:mb-4 md:rounded-3xl'
       />
-      <div className='lg:text-md mb-2 flex items-center space-x-3 text-sm'>
-        <div className='font-light text-accent-text dark:text-accent-text-dark'>
-          <DateFormatter dateString={post.date} />
-        </div>
+      <h3 className='4xl:text-2xl mb-4 text-center text-xl leading-tight tracking-tight group-hover/recent-post:text-link group-hover/recent-post:underline group-focus/recent-post:text-link group-focus/recent-post:underline dark:group-hover/recent-post:text-link-dark dark:group-focus/recent-post:text-link-dark xs:text-left xl:text-[2rem]'>
+        {post.title}
+      </h3>
+      <div className='flex flex-col items-center space-x-2 space-y-1 text-sm font-medium text-accent-text dark:text-accent-text-dark xs:flex-row xs:space-y-0 lg:text-base'>
+        <DateFormatter
+          dateString={post.date}
+          formatOptions={{ day: 'numeric', month: 'long', year: 'numeric' }}
+        />
         <Interpunct />
-        <div className='font-light uppercase text-accent-text dark:text-accent-text-dark'>
+        <div className='uppercase'>
           <CategoryFormatter categories={post.categories} />
         </div>
       </div>
-      <h3 className='4xl:text-2xl text-xl leading-tight tracking-tight group-hover/recent-post:text-link group-hover/recent-post:underline group-focus/recent-post:text-link group-focus/recent-post:underline dark:group-hover/recent-post:text-link-dark dark:group-focus/recent-post:text-link-dark xl:text-[2rem]'>
-        {post.title}
-      </h3>
     </Link>
   </div>
 )
