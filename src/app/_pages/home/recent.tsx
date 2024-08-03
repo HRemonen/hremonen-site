754c50d6b91b13254146e925e0e91e30c32b5254
Henri Remonen <henri@remonen.fi>
2024-08-03 09:55:29 +0300

    refactor: extract browse all posts section to own component

--- src/app/_pages/home/recent.tsx
+++ src/app/_pages/home/recent.tsx
@@ -1,4 +1,3 @@
-import ButtonLink from '@/app/_components/ui/button-link'
 import PageSection from '@/app/_components/ui/page-section'
 import SubSectionTitle from '@/app/_components/ui/page-sub-section-title'
 import PostCard from '@/app/_components/ui/post-card'
@@ -25,13 +24,6 @@ const RecentSection = () => {
           {posts.slice(2, 5).map((post) => (
             <PostCard key={post.slug} post={post} />
           ))}
-        </div>
-      </div>
-
-      <div className='flex flex-col px-4 md:flex-row'>
-        <SubSectionTitle title='All blog posts' subtitle='Browse all posts.' />
-        <div className='mb-12 flex w-full items-center justify-center md:mt-8 md:justify-normal lg:mb-16'>
-          <ButtonLink linkText='Check all posts' href='/blog' />
         </div>
       </div>
     </PageSection>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import PageContainer from '@/app/_components/ui/page-container'
 import FeaturedSection from '@/app/_pages/home/featured'
 import RecentSection from '@/app/_pages/home/recent'
+import BrowseSection from './_pages/home/browse'
 
 const Index = () => (
   <PageContainer>
     <FeaturedSection />
     <RecentSection />
+    <BrowseSection />
   </PageContainer>
 )
 
