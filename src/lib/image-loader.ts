506388e95fc834fec8fe29c7f32eb42f255bd5ed
Henri Remonen <henri@remonen.fi>
2024-08-09 10:17:42 +0300

    fix: add missing type declaration for the image loader

--- src/lib/image-loader.ts
+++ src/lib/image-loader.ts
@@ -1,6 +1,12 @@
 'use client'
 
-export default function cloudinaryLoader({ src, width, quality }: any) {
+import type { ImageLoaderProps } from 'next/image'
+
+export default function cloudinaryLoader({
+  src,
+  width,
+  quality,
+}: ImageLoaderProps) {
   const params = ['f_auto', 'c_limit', `w_${width}`, `q_${quality || 'auto'}`]
   return `https://res.cloudinary.com/daty4gssm/image/upload/${params.join(',')}${src}`
 }
